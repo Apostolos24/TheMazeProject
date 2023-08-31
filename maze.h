1a50b2eac80d1c899e5411f1ba6570c8c129bb83
Apostolos Skotis <106014620+Apostolos24@users.noreply.github.com>
2023-09-01 00:01:57 +0300

    Update maze.h
    Added getters

--- maze.h
+++ maze.h
@@ -20,9 +20,21 @@ private:
 public:
     Maze (int _rows, int _cols);
 
+    // getters
+    int get_rows();
+    int get_cols();
+    int get_start_x();
+    int get_start_y();
+    int get_end_x();
+    int get_end_y();
+    int get_total_tiles();
+    //
+
     void generate_random();
 
     friend std::ostream& operator<< (std::ostream& out,Maze& maze);
+
+    void print();
 
     };
 
